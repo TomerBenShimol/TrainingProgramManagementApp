5bf0e89febdc73d88e244afac3e9380f5de7f621
תומר בן שימול <tomerbenshimol@MacBook-Pro-sl-twmr.local>
2025-06-15 15:16:01 +0300

    backend initialization

---
+++ backend/src/main/java/tbs/backend/TrainingProgramManagementAppApplication.java
@@ -0,0 +1,13 @@
+package tbs.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingProgramManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingProgramManagementAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/tbs/backend/TrainingProgramManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package tbs.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingProgramManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
